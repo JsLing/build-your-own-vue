b1b18f4009aecee0a6f8e2bfaf39ac626592478b
JsLing <1569861870@qq.com>
2022-09-26 23:07:02 +0800

    feat: isReactive & isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -4,10 +4,23 @@ function createActiveObject(raw: any, baseHandlers: any) {
   return new Proxy(raw, baseHandlers)
 }
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__V_isReactive',
+  IS_READONLY = '__V_isReadonly',
+}
+
 export function reactive(raw: any) {
   return createActiveObject(raw, mutableHandlers)
 }
 
 export function readonly(raw: any) {
   return createActiveObject(raw, readonlyHandlers)
+}
+
+export function isReactive(value: any) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
+}
+
+export function isReadonly(value: any) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
 }
--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,11 +1,13 @@
-import { reactive, readonly } from "../reactive"
+import { isReactive, readonly, isReadonly } from '../reactive'
 
-describe("reactive", () => {
-  it("happy path", () => {
+describe('reactive', () => {
+  it('happy path', () => {
     const orginal = { test: 1 }
     // not set
     const observed = readonly(orginal)
     expect(observed).not.toBe(orginal)
     expect(observed.test).toBe(1)
+    expect(isReactive(observed)).toBe(true)
+    expect(isReactive(orginal)).toBe(false)
   })
 })
